d0a9b4729b2a542af2419f32489bf6c21aa8b1a2
J. Freiheit <freiheit@htw-berlin.de>
2023-01-25 20:00:34 +0100

    Auto-generated commit [ci skip]

--- src/app/shared/user.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-  username: string;
-  password: string;
-  email: string;
-  role: string;
-}
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://userbackend-cvf9.onrender.com/'
-};
